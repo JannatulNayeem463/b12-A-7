a8023e061b427cb667110479857522a175b35018
Jannatulnayeem <silpeislam463@example.com>
2025-09-26 19:11:51 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   const bannerPromies = fetchBanner()
   const cardPromies = fetchCard()
-
+/*-----*/
   
   const [cardData, setCardData] = useState([])
   const [taskStatusCards, setTaskStatusCards] = useState([])
